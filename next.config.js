4205f705196720be050e7fa2dfd0283b15d885ed
kingskuan <kingskuan@gmail.com>
2026-04-15 18:19:02 +0800

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { domains: ['avatars.githubusercontent.com', 'github.com', 'raw.githubusercontent.com'] },
-  experimental: { appDir: true }
+  images: {
+    domains: ['avatars.githubusercontent.com', 'github.com', 'raw.githubusercontent.com']
+  }
+  // Note: appDir is stable in Next.js 14, no need for experimental flag
 }
 module.exports = nextConfig
